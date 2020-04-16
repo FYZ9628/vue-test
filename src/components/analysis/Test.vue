f29a056fb4496eddb3b49863b5068e447c094c01
LAPTOP-HG3TISJO\tp <249046859@qq.com>
2020-04-16 22:53:28 +0800

    调整Test界面 修改数据线选择 增加处理方式选择

--- src/components/analysis/Test.vue
+++ src/components/analysis/Test.vue
@@ -49,50 +49,53 @@
           </el-row>
           <el-row  type="flex">
             <el-col :span="3">
-              <h4 class="login_title">维度:</h4>
+              <h3 class="login_title">维度:</h3>
             </el-col>
-            <el-col :span="20"><div class="grid-content bg-purple">
-              <div style="margin-top: 10px">
-                <el-checkbox-group v-model="checkboxGroup1" size="small">
-                  <el-checkbox label="日期" border></el-checkbox>
-                  <el-checkbox label="访问用户" border ></el-checkbox>
-                </el-checkbox-group>
-              </div>
-              <div style="margin-top: 10px">
-                <el-checkbox-group v-model="checkboxGroup2" size="small" >
-                  <el-checkbox label="下单用户" border></el-checkbox>
-                  <el-checkbox label="下单率" border></el-checkbox>
-                </el-checkbox-group>
-              </div>
+          </el-row>
+          <el-row>
+            <el-col :span="23"><div class="grid-content bg-purple">
+              <el-checkbox-group
+                v-model="checkboxGroup1"
+                max="1">
+                <el-checkbox-button v-for="option in checkboxOptions" :label="option" :key="option">{{option}}</el-checkbox-button>
+              </el-checkbox-group>
             </div></el-col>
           </el-row>
-          <el-row  type="flex" style="margin-top: 30px">
+          <el-row  type="flex">
             <el-col :span="3">
-              <h4 class="login_title">指标:</h4>
+              <h3 class="login_title">指标:</h3>
             </el-col>
-            <el-col :span="20"><div class="grid-content bg-purple">
-              <div style="margin-top: 20px">
-                <el-checkbox-group v-model="checkboxGroup3" size="small">
-                  <el-checkbox label="日期" border></el-checkbox>
-                  <el-checkbox label="访问用户" border ></el-checkbox>
-                </el-checkbox-group>
-              </div>
-              <div style="margin-top: 20px">
-                <el-checkbox-group v-model="checkboxGroup4" size="small" >
-                  <el-checkbox label="下单用户" border></el-checkbox>
-                  <el-checkbox label="下单率" border></el-checkbox>
-                </el-checkbox-group>
-              </div>
-            </div></el-col>
           </el-row>
-          <el-row  type="flex" style="margin-top: 30px">
-            <el-col :span="6" :offset="15">
-              <el-button type="primary">确定</el-button>
-            </el-col>
-
+          <el-row>
+            <el-col :span="23"><div class="grid-content bg-purple">
+              <el-checkbox-group v-model="checkboxGroup2">
+                <el-checkbox-button v-for="option in checkboxOptions" :label="option" :key="option">{{option}}</el-checkbox-button>
+              </el-checkbox-group>
+            </div></el-col>
           </el-row>
         </el-col>
 
+      </el-row>
+      <el-divider></el-divider>
+      <el-row style="height: 20%">
+        <el-col :span="24" class="grid-a-contentWidth">
+          <el-row type="flex">
+            <el-col :span="23" align="middle"><div class="grid-content bg-purple">
+              <h3 class="login_title">处理方式选择</h3>
+            </div></el-col>
+          </el-row>
+          <el-row  type="flex">
+            <el-col :span="6" :offset="2">
+              <el-button type="primary">常规</el-button>
+            </el-col>
+            <el-col :span="6" >
+              <el-button type="primary">求和</el-button>
+            </el-col>
+            <el-col :span="6" >
+              <el-button type="primary">求平均</el-button>
+            </el-col>
+          </el-row>
+        </el-col>
       </el-row>
     </el-aside>
     <el-main style="height: 100%">
@@ -195,57 +198,20 @@
         radio: '0',
         dialogVisible: false,
         loading: false,
-        preoutputs: [],
+        checkboxOptions:['日期', '访问用户', '下单用户'],
         filelist:[],
-        tableData: [{
-          date: '05-01',
-          access_user_num: '1393',
-          order_user_num:'1093',
-          order_rate:'0.32'
-        },{
-          date: '05-02',
-          access_user_num: '3530',
-          order_user_num:'3230',
-          order_rate:'0.26'
-        },{
-          date: '05-03',
-          access_user_num: '2923',
-          order_user_num:'2623',
-          order_rate:'0.76'
-        },{
-          date: '05-04',
-          access_user_num: '1723',
-          order_user_num:'1423',
-          order_rate:'0.49'
-        },{
-          date: '05-05',
-          access_user_num: '3792',
-          order_user_num:'3492',
-          order_rate:'0.323'
-        }],
-        // chartData: {
-        //   columns: ['日期', '访问用户', '下单用户'],
-        //   rows: [
-        //     { '日期': '05-01', '访问用户': 1393, '下单用户': 1093},
-        //     { '日期': '05-02', '访问用户': 3530, '下单用户': 3230},
-        //     { '日期': '05-03', '访问用户': 2923, '下单用户': 2623},
-        //     { '日期': '05-04', '访问用户': 1723, '下单用户': 1423},
-        //     { '日期': '05-05', '访问用户': 3792, '下单用户': 3492},
-        //     { '日期': '05-06', '访问用户': 4593, '下单用户': 4293}
-        //   ]
-        // },
+        chartData: {
+          columns: ['日期', '访问用户', '下单用户'],
+          rows: [
+            { '日期': '05-01', '访问用户': 1393, '下单用户': 1093},
+            { '日期': '05-02', '访问用户': 3530, '下单用户': 3230},
+            { '日期': '05-03', '访问用户': 2923, '下单用户': 2623},
+            { '日期': '05-04', '访问用户': 1723, '下单用户': 1423},
+            { '日期': '05-05', '访问用户': 3792, '下单用户': 3492},
+            { '日期': '05-06', '访问用户': 4593, '下单用户': 4293}
+          ]
+        },
         chartData:{},
-        // chartData: {
-        //   columns: ['日期', '访问用户', '下单用户', '下单率'],
-        //   rows: [
-        //     { '日期': '05-01', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-        //     { '日期': '05-02', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-        //     { '日期': '05-03', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-        //     { '日期': '05-04', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-        //     { '日期': '05-05', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-        //     { '日期': '05-06', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
-        //   ]
-        // },
         chartSettings: {
           type: this.typeArr[this.index],
             // metrics: ['访问用户', '下单用户'],
@@ -253,16 +219,6 @@
         }
       }
     },
-    // computed:{
-    //   myThs:function(){
-    //     var ths = [];
-    //     for    (var k in chartData){
-    //       ths.push(k);
-    //       console.log(k)
-    //     }
-    //     return ths;
-    //   }
-    // },
     methods: {
 
       //改变图表类型
